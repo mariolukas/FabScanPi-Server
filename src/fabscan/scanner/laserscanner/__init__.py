49db74844e26150c130244f1a64ce3635c391d71
Mario Lukas <info@mariolukas.de>
2016-08-31 08:03:48 +0200

    refactored scanner intit

---
+++ src/fabscan/scanner/laserscanner/__init__.py
@@ -0,0 +1,19 @@
+__author__ = "Mario Lukas"
+__copyright__ = "Copyright 2015"
+__license__ = "AGPL"
+__maintainer__ = "Mario Lukas"
+__email__ = "info@mariolukas.de"
+
+from fabscan.util.FSInject import injector
+from fabscan.scanner.interfaces.FSScanProcessor import FSScanProcessorInterface
+from fabscan.scanner.interfaces.FSHardwareController import FSHardwareControllerInterface
+from fabscan.scanner.interfaces.FSImageProcessor import ImageProcessorInterface
+from fabscan.scanner.laserscanner.FSScanProcessor import FSScanProcessorSingleton
+from fabscan.scanner.laserscanner.FSHardwareController import FSHardwareControllerSingleton
+from fabscan.scanner.laserscanner.FSImageProcessor import ImageProcessor
+
+def create():
+    # "dynamic" module classes ...
+    injector.provide(ImageProcessorInterface, ImageProcessor)
+    injector.provide(FSHardwareControllerInterface, FSHardwareControllerSingleton)
+    injector.provide(FSScanProcessorInterface, FSScanProcessorSingleton)
