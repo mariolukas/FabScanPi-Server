a7a5a437ff925afa80419f85321bdeb50a1845c2
Mario Lukas <info@mariolukas.de>
2016-09-07 23:52:10 +0200

    fixed developer docs

--- docs/developing.md
+++ docs/developing.md
@@ -2,7 +2,7 @@
 The FabScan PI server code is located in this repository. The folder src/www contains the latest version of the 
 FabScan PI web interface. 
 
-Install build dependencies with ```apt-get install build-essential python-setuptools debhelper cdbs```
+Install build dependencies with ```apt-get install build-essential python-setuptools debhelper cdbs dh-python```
 
 Afterwards the deb files can be build by calling ```make deb```
 
--- setup.py
+++ setup.py
@@ -109,7 +109,7 @@ def params():
         ('/usr/local/fabscanpi/www/js/',['src/www/js/app.js', 'src/www/js/lib.js']),
         ('/usr/local/fabscanpi/www/js/locales/en/',['src/www/js/locales/en/i18n.js']),
         ('/usr/local/fabscanpi/www/js/locales/de/',['src/www/js/locales/de/i18n.js']),
-        ('/usr/local/fabscanpi/www/icons/', ['src/www/icons/icon_mesh.svg','src/www/icons/icon_scan.svg','src/www/icons/icon_pointcloud.svg','src/www/icons/favicon.png', 'src/www/icons/spinner.gif']),
+        ('/usr/local/fabscanpi/www/icons/', ['src/www/icons/icon_mesh.svg','src/www/icons/icon_scan.svg','src/www/icons/icon_pointcloud.svg','src/www/icons/favicon.png', 'src/www/icons/spinner.gif', 'src/www/icons/logo.png']),
         ('/usr/local/fabscanpi/www/fonts/', ['src/www/fonts/fontawesome-webfont.woff2', 'src/www/fonts/fontawesome-webfont.woff', 'src/www/fonts/fontawesome-webfont.ttf']),
         ('/usr/local/fabscanpi/www/style/fonts/', ['src/www/style/fonts/slick.woff', 'src/www/style/fonts/slick.ttf'])
 
